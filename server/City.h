04d796d4419f6cf2edb9fd46c041644d3162db85
Ulises <utripaldi@fi.uba.ar>
2025-10-29 18:22:50 -0300

    Creo la clase City para obtener los objetos estaticos y los sensores de cabio de nivel para puentes.

---
+++ server/City.h
@@ -0,0 +1,42 @@
+#ifndef CITY_H
+#define CITY_H
+
+#include <string>
+#include <vector>
+
+enum CityName { LibertyCity, SanAndreas, ViceCity };
+
+struct StaticObjectData {
+    float x;
+    float y;
+    float width;
+    float height;
+    bool isUp;
+};
+
+struct BridgeSensorData {
+    float x;
+    float y;
+    float width;
+    float height;
+};
+
+class City {
+private:
+    CityName name;
+    std::vector<StaticObjectData> staticObjects;
+    std::vector<BridgeSensorData> bridgeSensors;
+
+    std::string getYamlFileName() const;
+
+public:
+    explicit City(CityName name);
+    ~City();
+
+    const std::vector<StaticObjectData> getStaticObjects() const;
+
+    const std::vector<BridgeSensorData>& getBridgeSensors() const;
+};
+
+
+#endif
